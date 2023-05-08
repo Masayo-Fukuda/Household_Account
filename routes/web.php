a427d8ac2daac8dc2a0616ad5c7d67b200747aec
Masayo-Fukuda <126930383+Masayo-Fukuda@users.noreply.github.com>
2023-05-08 09:28:54 +0900

    create files

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RecordController;
+use App\Http\Controllers\MyPageController;  
+use App\Http\Controllers\DayController;  
+use App\Http\Controllers\MonthController;
+use App\Http\Controllers\ExchangeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +19,57 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::get('top', function () {
+    return view('top');
+});
+
+Route::get('login_success', function () {
+    return view('login_success');
+});
+
+Route::get('register_success', function () {
+    return view('register_success');
+});
+
+Route::group(['middleware' => ['auth']], function () {
+
+    // 保護されたルートの定義
+    Route::get('/protected-page', [ProtectedController::class, 'index']);
+
+    //Record
+    Route::resource('records', RecordController::class);
+
+    Route::get('/records/edit/option={option}/id={id}', [RecordController::class, 'edit'])->name('records.edit');
+
+    Route::delete('/records/{option}/{id}', [RecordController::class, 'destroy'])->name('records.destroy');
+
+    //My Page
+    Route::get('/my_page/{id}', [MyPageController::class, 'show'])->name('my_page.show');
+
+    //Day
+    Route::get('day', [DayController::class, 'index'])->name('day.index');
+
+    Route::get('/get-day', [DayController::class, 'getData']);
+
+    Route::get('/prev-week', [DayController::class, 'getPrevWeek']);
+
+    Route::get('/next-week', [DayController::class, 'getNextWeek']);
+
+    //Month
+    Route::get('month', [MonthController::class, 'index'])->name('month.index');
+
+    Route::get('/get-month', [MonthController::class, 'getData']);
+
+    Route::get('/prev-month', [MonthController::class, 'getPrevMonth']);
+    
+    Route::get('/next-month', [MonthController::class, 'getNextMonth']);
+
+    //API
+    Route::get('/exchange-rate', [ExchangeController::class, 'getExchangeRate']);
 });
